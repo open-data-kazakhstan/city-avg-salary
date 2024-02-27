8a5a11a1af092128613f0fb72f6b8894109a66b1
Spuf0Aine <toktabaevaanbek@gmail.com>
2024-02-27 10:19:56 +0600

    isues fix

--- Scripts/datapac.py
+++
@@ -1,6 +0,0 @@
-from datapackage import Package
-
-package = Package()
-package.infer(r"data\avg_salary_.csv")
-package.commit()
-package.save(r"../datapackage.json")
--- Scripts/process.py
+++ Scripts/process.py
@@ -1,6 +1,7 @@
 import openpyxl
 import csv
 import os
+from datapackage import Package
 
 
 def converter(input_excel_file, output_csv_file):
@@ -23,6 +24,7 @@ converter(input_excel_file, first_out)
 
 def renamer(input_file, output_file):
     column_mapping = {
+        'Регионы': 'regions',
         'Всего персонала': 'avg_salary',
         'руководители и государственные служащие': 'managers_&_civil_servants',
         'специалисты-профессионалы': 'qualified',
@@ -30,9 +32,9 @@ def renamer(input_file, output_file):
         'служащие в области администрирования': 'admin_emplyees',
         'работники сферы услуг и продаж': 'service_and_sales',
         'фермеры и рабочие сельского и лесного хозяйства, рыбоводства и рыболовства': 'agriculture',
-        'рабочие промышленности, строительства, транспорта и других родственных занятий': 'transport',
-        'операторы производственного оборудования, сборщики и водители': 'unqualified',
-        'неквалифицированные рабочие': 'not_members_of_other_groups',
+        'рабочие промышленности, строительства, транспорта и других родственных занятий': 'Engineers_transport',
+        'операторы производственного оборудования, сборщики и водители': 'assemblers_and_drivers',
+        'неквалифицированные рабочие': 'unqualified',
         'работники, не входящие в другие группы ': 'not_members_of_other_groups'
     }
 
@@ -50,13 +52,13 @@ def renamer(input_file, output_file):
 
 def text_remover(input_file, output_file):
     with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', encoding='utf-8', newline='') as outfile:
-        # Flag to skip lines
+
         skip_line = False
 
         for line in infile:
             if "16. Среднемесячная заработная" in line or "тенге" in line:
                 skip_line = True
-                continue  # Skip this line
+                continue
             if not skip_line:
                 outfile.write(line)
             skip_line = False
@@ -72,7 +74,84 @@ def delete_file(file_path):
         print(f"Error deleting file '{file_path}': {e}")
 
 
+def create_datapackage(data_file, metadata_file='datapackage.json'):
+    package = Package()
+
+    resource = package.add_resource({"path": "data/avg_salary_.csv",
+            "profile": "data-resource",
+            "name": "avg_salary_",
+            "format": "csv",
+            "mediatype": "text/csv",
+            "encoding": "utf-8",
+            "schema": {
+                "fields":[
+            {
+                "name": "regions",
+                "type": "string",
+                "format": "default"
+            },
+                    {
+                "name": "avg_salary",
+                "type": "integer",
+                "format": "default"
+            },
+                    {
+                "name": "managers_&_civil_servants",
+                "type": "integer",
+                "format": "default"
+            },
+                    {
+                "name": "qualified",
+                "type": "integer",
+                "format": "default"
+            },
+                    {
+                "name": "tech_personnel",
+                "type": "integer",
+                "format": "default"
+            },
+                    {
+                "name": "admin_emplyees",
+                "type": "integer",
+                "format": "default"
+            },
+                    {
+                "name": "service_and_sales",
+                "type": "integer",
+                "format": "default"
+            },
+                    {
+                "name": "agriculture",
+                "type": "integer",
+                "format": "default"
+            },
+                    {
+                "name": "transport",
+                "type": "integer",
+                "format": "default"
+            },
+                    {
+                "name": "assemblers_and_drivers",
+                "type": "integer",
+                "format": "default"
+            },{
+                "name": "unqualified",
+                "type": "integer",
+                "format": "default"
+            },{
+                "name": "not_members_of_other_groups",
+                "type": "integer",
+                "format": "default"
+            }
+                ]}})
+
+    resource.infer()
+
+    package.save(metadata_file)
+    print(f"Datapackage created! Check '{metadata_file}' ")
+
+
 second_out = 'data/avg_salary_.csv'
 renamer(first_out, second_out)
 delete_file(first_out)
-
+create_datapackage(second_out)
